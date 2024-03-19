c3e7f7056406924493160cd7e581928124652fc9
ahmadihsanullah <ahma21327ti@student.nurulfikri.ac.id>
2024-03-19 07:54:39 +0700

    add entity category

--- database.sql
+++ database.sql
@@ -3,3 +3,11 @@ CREATE DATABASE belajar_spring_data_jpa
     DEFAULT CHARACTER SET = 'utf8mb4';
 
 use belajar_spring_data_jpa;
+
+CREATE TABLE categories(
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+show tables;
